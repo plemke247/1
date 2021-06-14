c6fd199a278412456562a3a77a6b7933f6f231db
plemke247 <84066078+plemke247@users.noreply.github.com>
2021-06-14 21:56:00 +0200

    Add files via upload

---
+++ 640px-NLC-Wolken-Müritz.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="640px-NLC-Wolken-Müritz" tilewidth="32" tileheight="32" tilecount="260" columns="20">
+ <image source="../640px-NLC-Wolken-Müritz.jpg" width="640" height="427"/>
+</tileset>
