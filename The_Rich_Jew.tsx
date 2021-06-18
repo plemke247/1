dcdcd8551bc5c9f121f3b7e9fbc4f7ec800c3521
plemke247 <84066078+plemke247@users.noreply.github.com>
2021-06-18 09:25:18 +0200

    Add files via upload

---
+++ The_Rich_Jew.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="The_Rich_Jew" tilewidth="32" tileheight="32" tilecount="180" columns="12">
+ <image source="The_Rich_Jew.jpg" width="400" height="500"/>
+</tileset>
